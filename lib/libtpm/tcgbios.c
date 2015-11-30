dc171d4b1b64729541b34e325460f1b7f85acf33
Stefan Berger <stefanb@linux.vnet.ibm.com>
2015-11-30 16:54:47 -0500

    tpm: Set the driver in pseudo failure state after handover
    Once sml-handover has been called, set the driver into
    pseudo failure state.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>

--- lib/libtpm/tcgbios.c
+++ lib/libtpm/tcgbios.c
@@ -928,3 +928,15 @@ uint32_t tpm_driver_get_failure_reason(void)
 
 	return spapr_vtpm_get_error();
 }
+
+/*
+ * tpm_driver_set_failure_reason: Function for interfacing with the firmware
+ *                                API
+ */
+void tpm_driver_set_failure_reason(uint32_t errcode)
+{
+	if (!tpm_state.tpm_found)
+		return;
+
+	spapr_vtpm_set_error(errcode);
+}
