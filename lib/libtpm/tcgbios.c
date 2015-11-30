18e17b0f04dd84d4f1185fc2bc644525db4edb3c
Stefan Berger <stefanb@linux.vnet.ibm.com>
2015-11-30 16:54:47 -0500

    Add TPM firmware API call get-state
    This patch adds support for the TPM firmware API call get-state.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>

--- lib/libtpm/tcgbios.c
+++ lib/libtpm/tcgbios.c
@@ -149,6 +149,18 @@ bool tpm_is_working(void)
 	return tpm_state.tpm_working;
 }
 
+/*
+ * tpm_driver_get_state: Function for interfacing with the firmware API
+ */
+uint32_t tpm_driver_get_state(void)
+{
+	/* do not check for a working TPM here */
+	if (!tpm_state.tpm_found)
+		return VTPM_DRV_STATE_INVALID;
+
+	return spapr_vtpm_get_state();
+}
+
 /* Helper function for sending TPM commands that take a single 
  * optional parameter (0, 1, or 2 bytes) and have no special response.
  */
