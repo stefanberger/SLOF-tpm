8995f5a4d24a58e998010a2bb0b65cb49edddb29
Stefan Berger <stefanb@linux.vnet.ibm.com>
2015-11-30 16:54:47 -0500

    Add TPM firmware API call get-failure-reason
    This patch adds support for the TPM firmware API call get-failure-reason.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>

--- lib/libtpm/tcgbios.c
+++ lib/libtpm/tcgbios.c
@@ -208,6 +208,19 @@ uint32_t tpm_driver_get_state(void)
 	return spapr_vtpm_get_state();
 }
 
+/*
+ * tpm_driver_get_failure_reason: Function for interfacing with the firmware
+ *                                API
+ */
+uint32_t tpm_driver_get_failure_reason(void)
+{
+	/* do not check for a working TPM here */
+	if (!tpm_state.tpm_found)
+		return VTPM_DRV_STATE_INVALID;
+
+	return spapr_vtpm_get_error();
+}
+
 static uint32_t transmit(struct tpm_req_header *req,
 			 uint8_t *respbuffer, uint32_t *respbufferlen,
 			 enum tpm_duration_type to_t)
