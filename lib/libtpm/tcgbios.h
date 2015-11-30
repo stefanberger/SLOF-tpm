9cf9a5aa75d04afd0e043bf723c710ab5009ad14
Stefan Berger <stefanb@linux.vnet.ibm.com>
2015-11-30 16:54:47 -0500

    tpm: Add TPM initialization support
    This patch implements the main part of the firmware extensions. It provides
    the following functionality:
    
    - initialization of the TPM by sending a sequence of commands to it
    - proper setup of the TPM before the firmware hands over control to the bootloader
    
    Structures that are needed in subsequent patches are also included in the
    private header file tcgbios_int.h at this point.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

---
+++ lib/libtpm/tcgbios.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ * Copyright (c) 2015 IBM Corporation
+ * All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Contributors:
+ *     IBM Corporation - initial implementation
+ *****************************************************************************/
+
+#ifndef TCGBIOS_H
+#define TCGBIOS_H
+
+#include <stdint.h>
+
+uint32_t tpm_start(void);
+void tpm_finalize(void);
+uint32_t tpm_unassert_physical_presence(void);
+
+#endif /* TCGBIOS_H */
